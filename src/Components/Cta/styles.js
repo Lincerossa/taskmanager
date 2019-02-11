47023ed031523e1632a251920521d50f5b7ece81
Lincerossa90 <marcello.luatti@alkemy.com>
2019-02-11 19:05:54 +0100

    [config] test config

--- src/Components/Cta/styles.js
+++ src/Components/Cta/styles.js
@@ -1,26 +1,26 @@
 import styled from 'styled-components'
 
 export const Cta = styled.a`
-    padding: .75rem;
-    background-color: white;
-    border-radius: 25px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-decoration: none;
-    font-family: sans-serif;
-    color: black;
-    min-width: 125px;
-    font-size: 1rem;
-    border: 2px solid black;
-    cursor: pointer;
+                                                                                              padding: .75rem;
+                                                                                              background-color: white;
+                                                                                              border-radius: 25px;
+                                                                                              display: flex;
+                                                                                              align-items: center;
+                                                                                              justify-content: center;
+                                                                                              text-decoration: none;
+                                                                                              font-family: sans-serif;
+                                                                                              color: black;
+                                                                                              min-width: 125px;
+                                                                                              font-size: 1rem;
+                                                                                              border: 2px solid black;
+                                                                                              cursor: pointer;
 
-    &:hover{
-      background-color: black;
-      color:  white;
-    }
+                                                                                              &:hover{
+                                                                                                background-color: black;
+                                                                                                color:  white;
+                                                                                              }
 
-    @media screen and (min-width: 768px){
-      min-width: 200px;
-    }
+                                                                                              @media screen and (min-width: 768px){
+                                                                                                min-width: 200px;
+                                                                                              }
 `
